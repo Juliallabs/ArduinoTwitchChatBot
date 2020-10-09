102f0720763dfccccb312e87241fd8877da5587f
RodrigoDornelles <rodrigo@dornelles.me>
2020-10-09 00:47:43 -0300

    minor improves & code formatting

--- chatbot.js
+++ chatbot.js
@@ -1,127 +1,86 @@
-tmi = require('tmi.js')
- 
-const BOT_NOME = 'Eletro-Julia';
-const BOT_CHAVE = 'sua chave da twitch aqui'
+/**
+ * @description Importaçao de blibiotecas
+ * 
+ * @requires johnny-five
+ * @requires tmi.js
+ * @requires fs
+ * 
+ */
+const { Board, Piezo, Led, Pin } = require("johnny-five");
+const tmi = require('tmi.js')
+const fs = require('fs');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------
-//-------------------------------------------_CHAVEEEE------------------------------------------
-
-
-
-
-
-
-var five = require("johnny-five");
-//var board = new five.Board();
-const { Board, Led } = require("johnny-five");
-
+/**
+ * @description Construção de Objetos
+ */
 const board = new Board();
+const options = JSON.parse(fs.readFileSync('options.json'));
 
-
-
-const options = {
-  
-  options:{
-    debug: true 
-   },
-
-identity:{
-  username: BOT_NOME,
-  password: BOT_CHAVE
-},
-
-channels: ['julialabs']
-
-};
-
+/**
+ * @description Eventos (Funções Callback)
+ * @throws Erro quando arduino não conectado corretamente
+ */
 board.on("ready", function() {
-  const pino = new Led(11);
 
-  const client = new tmi.client(options);
-  client.connect();
-  
-  /*client.on('message',function(channel,userstate,message,self){
-  
-    if (self) return;
-    //console.log(message);
+	/**
+	 * @description Configuração do micro controlador
+	 */
+	const pino = new Led(11);
+	const piezo = {
+		vcc: new Pin(5),
+		gnd: new Pin(7),
+		pin: new Piezo(6)
+	};
 
-  });
-*/
-  client.on("raw_message", (messageCloned, message) => {
-   // console.log(message);
-      if( message.tags["msg-id"]== 'highlighted-message'){ 
-       
-        if('!liga' == message.params[1]){
-          client.say (message.params[0], "ELETROBLOCKS ON")
-          pino.pulse();
-          
-        }
-      
-        if ('!desliga'== message.params[1]){
-          client.say (message.params[0], "ELETROBLOCKS OFF")
-          pino.stop().off();    
-      
-        }
-      }
+	/**
+	 * @description Configuração e Conexão com API da Twitch.TV
+	 */
+	const client = new tmi.client(options);
+	client.connect();
+
+	/**
+	 * @description Quando menssagem enviada
+	 * @see raw_message tem os dados brutos de qualquer evento de chat da live (messangem, highlight, host, raid)
+	 */
+	client.on("raw_message", (messageCloned, message) => {
+		
+		// ignorar quando não for uma menssagem no chat
+		if (message.command != "PRIVMSG") {
+			return;
+		}
+
+		// preparar para executar comandos
+		let is_highlight = message.tags["msg-id"] == 'highlighted-message';
+		let channel = message.params[0];
+		let params = message.params[1].slice(1).split(' ');
+		let command = params.shift().toLowerCase(); 
+	 
+		if (command == "rei") {
+			// confirmar no chat o comando que recebeu, enviando uma mensagem!
+			client.say (channel, "IHA!");
+
+			// isso foi usado para conectar o modulo buzzer direto no arduino,
+			// pode ser removido caso usar alimentação com vcc e gnd ao invez das GPIO's
+			piezo.vcc.high();
+			piezo.gnd.low();
+
+			// tocar música rei do gado
+			piezo.pin.play({tempo: 156, song: [["D5",  1],[null,  1],["F5",  1],[null,  1],["A#5", 1],[null,  1],["D6",  1],[null,  1],["A#5", 1],[null,  1],["F5",  1],[null,  1],["G5",  1],[null,  3]["D6",  1],[null,  3],["C6",  1],[null,  3],["D6",  1],[null,  1]]});
+		}
+
+		if (command == "stop") {
+			piezo.pin.off();
+		}
+		
+		// comando que requer destacar a menssagem
+		if (command == "liga" && is_highlight) {
+			client.say (message.params[0], "ELETROBLOCKS ON")
+			pino.pulse();
+		}
+		
+		if (command == "desliga" && is_highlight) {
+			client.say (message.params[0], "ELETROBLOCKS OFF")
+			pino.stop().off();    
+		}
+	});
 });
-
-    
-//'msg-id': 'highlighted-message',
-// message.tags
-  
-    /*client.on('join', (channel, username, selfs) => {
-    
-      if (self) return;
-
-      client.say (channel,"bem vindo ao Lab " + username)
-  
-      pino.pulse();
-  
-      board.wait(1000, () => {
-        pino.stop().off();
-      });
-  
-  });*/
-  
-
-
-});
-
